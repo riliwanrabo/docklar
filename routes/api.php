bb5ba8abf0eef16361ad17cb61069c3fd6e2d1f6
Riliwan Balogun <riliwan.rabo@gmail.com>
2022-11-09 08:36:41 +0100

    health endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('health', function () {
+    return response()->json([
+        'status' => __('ok'),
+        'app_name' => env('APP_NAME'),
+        'php_version' => PHP_VERSION,
+        'app_version' => app()->version(),
+        'in_maintainance_mode' => app()->isDownForMaintenance(),
+        'locale' => app()->getLocale(),
+        'message' => __('up and running'),
+    ]);
 });
